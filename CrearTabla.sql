7dcf2d8f947c533b01037472f97de65dd8d13b11
catherinmb2019-tech <catherinmb2019@gmail.com>
2025-08-26 19:51:02 -0600

    Create CrearTabla.sql

---
+++ CrearTabla.sql
@@ -0,0 +1,18 @@
+/****** Object:  Table [dbo].[Empleado]    Script Date: 26/8/2025 19:50:31 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Empleado](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](128) NOT NULL,
+	[Salario] [money] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
