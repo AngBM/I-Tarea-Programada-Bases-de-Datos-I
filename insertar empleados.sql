0f77198a7ebf449c40f5685e59fa3a0e4a9ac087
catherinmb2019-tech <catherinmb2019@gmail.com>
2025-08-26 20:03:38 -0600

    Create insertar empleados.sql

---
+++ insertar empleados.sql
@@ -0,0 +1,11 @@
+USE [Tarea1 BD]
+GO
+
+INSERT INTO [dbo].[Empleado]
+           ([Nombre]
+           ,[Salario])
+     VALUES
+           (<Nombre, varchar(128),>
+           ,<Salario, money,>)
+GO
+
