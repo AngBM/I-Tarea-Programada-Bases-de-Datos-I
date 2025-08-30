6cfbf54552d68d2f5c8dbb9272b8f85fe3839e5a
catherinmb2019-tech <catherinmb2019@gmail.com>
2025-08-30 17:41:41 -0600

    pruebas de python

---
+++ prueba conexion.py
@@ -0,0 +1,19 @@
+import pyodbc
+
+
+connection = pyodbc.connect(
+    "DRIVER={ODBC Driver 18 for SQL Server};"
+    "SERVER=tcp:servidorbdcmb19.database.windows.net,1433;"
+    "DATABASE=Tarea1 BD;"
+    "UID=AdminCMB19;"
+    "PWD=adminbdCMB05;"
+    "Encrypt=yes;"
+    "TrustServerCertificate=no;"
+    "Connection Timeout=30;"
+)
+
+print(" Conexión exitosa")
+cursor = connection.cursor()
+cursor.execute("EXEC ObtenerEmpleados")
+for row in cursor.fetchall():
+    print(row)
