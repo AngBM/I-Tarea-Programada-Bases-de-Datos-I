ec0072f14ee419a606cb3714a15091c656d0388a
catherinmb2019-tech <catherinmb2019@gmail.com>
2025-09-07 17:18:28 -0600

    paso de codigo de un archivo aparte
    Mi persona Catherin habia realizado pruebas de html y flash en otro repositorio ahora lo pase a este

--- conexion.py
+++ conexion.py
@@ -1,24 +1,84 @@
-from flask import Flask, render_template, request
+from flask import Flask , render_template, request, redirect, url_for, flash
 import pyodbc
+import re
 
-app = Flask(__name__)
-
-# Conexión
-conn_str = (
+connection = pyodbc.connect(
     "DRIVER={ODBC Driver 18 for SQL Server};"
     "SERVER=tcp:servidorbdcmb19.database.windows.net,1433;"
-    "DATABASE=Tarea1 BD;"     
+    "DATABASE=Tarea1 BD;"
     "UID=AdminCMB19;"
     "PWD=adminbdCMB05;"
     "Encrypt=yes;"
     "TrustServerCertificate=no;"
-    "Connection Timeout=30;"
+    "Connection Timeout=60;"
 )
 
-def get_connection():
-    return pyodbc.connect(conn_str)
+print(" Conexión exitosa")
+def obtener_empleados():
+    
+    cursor = connection.cursor()            # Abrir cursor para ejecutar SQL
+    cursor.execute("EXEC ObtenerEmpleados1") # ejecuta el SP 
+    resultados = cursor.fetchall()          #  Trae todos los resultados
+    empleados = []                          # Crea lista vacía para guardar empleados
+    for fila in resultados:                 #  Recorre cada fila del resultado
+        empleados.append({
+            "id": fila[0],
+            "nombre": fila[1],
+            "salario": fila[2]
+        })
+    return empleados    
 
-@app.route("/", methods=["GET", "POST"])
+
+
+def insertar_empleado(nombre, salario):
+    try:
+        cursor = connection.cursor()                                #  Abre cursor
+        cursor.execute(
+            "EXEC InsertEmpleado1 @Nombre=?, @Salario=?",            #  ejecuta  el SP con  los parámetros
+            (nombre, salario)                                       
+        )
+        connection.commit()                                         # Confirmar los cambios en la BD
+        return "Empleado insertado"  
+    except pyodbc.Error as e:
+         raise Exception(e.args[1])                               
+
+
+#-------------------------------------------------------------------------------------
+#para ver todos los empleados
+app = Flask(__name__)
+
+@app.route("/")  # ruta principal
+def pantalla_principal():
+    empleados=obtener_empleados()
+    return render_template ("index.html",empleados=empleados)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#@app.route("/", methods=["GET", "POST"])
 def index():
     mensaje = ""
     if request.method == "POST":
@@ -46,7 +106,7 @@ def index():
     cursor.execute("EXEC dbo.ObtenerEmpleados1")
     empleados = cursor.fetchall()
 
-    return render_template("index.html", empleados=empleados, mensaje=mensaje)
+    return render_template("index.html", empleados=empleados, mensaje=mensaje) 
 
 if __name__ == "__main__":
     app.run(debug=True)
